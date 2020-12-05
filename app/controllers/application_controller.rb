2a6adda350ed13ad11059d486b96e12d1804ead3
jessicagomesng <jgnpeluso@gmail.com>
2020-12-05 15:10:46 +0000

    built out user routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::Base
+    helper_method :login!
+
+    def login! 
+        session[:user_id] = @user.user_id
+    end 
+
+    def logged_in?
+        !!session[:user_id]
+    end 
+
+    def current_user 
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end 
+
+    def authorised_user?
+        @user == curent_user 
+    end 
+
+    def logout!
+        session.clear 
+    end 
+
+    def set_user
+        @user = User.find_by_id(session[:user_id])
+    end 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in' to 'sessions#is_logged_in?'
 end
